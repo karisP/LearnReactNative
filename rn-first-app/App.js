1279d919c7762e8dd08e88fbfe206085ba288a2d
Karissa <phillips.karissa00@gmail.com>
2020-01-18 23:31:51 -0500

    added all files AND folders to commit

---
+++ rn-first-app/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { key: Math.random().toString(), value: goalTitle}]);
+  }
+
+
+  return (
+    <View style={styles.screen}>
+      <GoalInput onAddGoal={addGoalHandler}/>
+      <FlatList 
+        data={courseGoals} 
+        renderItem={itemData => (
+          <GoalItem title={itemData.item.value} />
+        )}/>
+        <View>
+        </View>
+  
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+});
